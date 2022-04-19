eb8f6738eb93a4e26312306ad97ab224594599e2
chelliahlem98gmail.com <chelliahlem98@gmail.com>
2022-04-19 10:01:43 +0100

    GetUsers Information

--- backend/controllers/activeDirectory.controller.js
+++ backend/controllers/activeDirectory.controller.js
@@ -1,18 +1,29 @@
-const find = require('local-devices');
+// const find = require('local-devices');
+const si = require('systeminformation');
+// // i use a modul for scan all the ip addres in the network
+// // https://github.com/DylanPiercey/local-devices
+// // TODO
+// //rest to return  liste 
+ exports.FindIp = (req, res, next) => {
+  console.log("cpu ")
 
-exports.FindIp = (req, res, next) => {
-  console.log("ip adresses")
-
-  // Without using a transpiler
  
+si.cpu()
+.then(data => console.log(data))
+.catch(error => console.error(error));
+
+
+//   // Find all local network devices.
+//   find().then((devices) => {
+
+//     console.log(devices);
+
+//   });
   
-  // Find all local network devices.
-  find().then((devices) => {
-    console.log(devices);
-  });
-  
+
+
+// promises style - new since version 3
 
 
 
-
-};
+ }
